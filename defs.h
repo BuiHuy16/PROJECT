67c99668a01342e9f7ff09ef2858e6bfdc895860
BuiHuy16 <bchuy331@gmail.com>
2025-03-17 16:19:02 +0700

    Delete defs.h

--- defs.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEFS_H_INCLUDED
-#define DEFS_H_INCLUDED
-
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
-
-
-#endif // DEFS_H_INCLUDED
