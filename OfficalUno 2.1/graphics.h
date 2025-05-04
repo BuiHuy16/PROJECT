97d13a1114529587e4b78421087552793d4096c9
BuiHuy16 <bchuy331@gmail.com>
2025-05-05 00:43:12 +0700

    Add files via upload

---
+++ OfficalUno 2.1/graphics.h
@@ -0,0 +1,191 @@
+#ifndef GRAPHICS_H_INCLUDED
+#define GRAPHICS_H_INCLUDED
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include "defs.h"
+#include <string>
+using namespace std;
+
+
+void logErrorAndExit(const char* msg, const char* error)
+{
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+    SDL_Quit();
+}
+SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logErrorAndExit("SDL_Init", SDL_GetError());
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+    return window;
+}
+
+SDL_Renderer* createRenderer(SDL_Window* window)
+{
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    return renderer;
+}
+SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+SDL_Renderer* renderer = createRenderer(window);
+
+void init() {
+    if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+    {
+        logErrorAndExit( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+    }
+}
+
+void prepareScene(SDL_Texture * background)
+{
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy( renderer, background, NULL, NULL);
+}
+
+void fillColor(int r, int g, int b, int a)
+{
+    SDL_SetRenderDrawColor(renderer, r, g, b, a);
+    SDL_RenderClear(renderer);
+}
+
+void presentScene()
+{
+    SDL_RenderPresent(renderer);
+}
+
+SDL_Texture* loadTexture(const string& filename)
+{
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename.c_str());
+
+    SDL_Texture* texture = IMG_LoadTexture(renderer, filename.c_str());
+    if (texture == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+
+    return texture;
+}
+
+void renderTexture(SDL_Texture *texture, int x, int y)
+{
+    SDL_Rect dest;
+
+    dest.x = x;
+    dest.y = y;
+    SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+    SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+void DrawScaled(SDL_Texture* texture, int x, int y, int w, int h) {
+    SDL_Rect dstRect = {x, y, w, h};
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+}
+
+void blitRect(SDL_Texture *texture, SDL_Rect *src, int x, int y)
+{
+    SDL_Rect dest;
+
+    dest.x = x;
+    dest.y = y;
+    dest.w = src->w;
+    dest.h = src->h;
+
+    SDL_RenderCopy(renderer, texture, src, &dest);
+}
+
+void DrawFilledCircle(int x, int y, int radius) {
+    for (int w = 0; w < radius * 2; w++) {
+        for (int h = 0; h < radius * 2; h++) {
+            int dx = radius - w; // Khoảng cách theo trục x
+            int dy = radius - h; // Khoảng cách theo trục y
+            if ((dx * dx + dy * dy) <= (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+            }
+        }
+    }
+}
+
+
+TTF_Font* loadFont(const char* path, int size)
+{
+    if (TTF_Init() == -1) {
+        logErrorAndExit("SDL_ttf could not initialize! SDL_ttf Error: ", TTF_GetError());
+    }
+    TTF_Font* gFont = TTF_OpenFont( path, size );
+    if (gFont == nullptr) {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load font %s", TTF_GetError());
+    }
+    else return gFont;
+}
+
+SDL_Texture* renderText(string text, TTF_Font* font, SDL_Color textColor)
+{
+    SDL_Surface* textSurface = TTF_RenderText_Solid( font, text.c_str(), textColor );
+    if( textSurface == nullptr ) {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Render text surface %s", TTF_GetError());
+        return nullptr;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface( renderer, textSurface );
+    if( texture == nullptr ) {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Create texture from text %s", SDL_GetError());
+    }
+
+    SDL_FreeSurface( textSurface );
+    return texture;
+}
+void quitSDL()
+{
+    Mix_Quit();
+    TTF_Quit();
+    IMG_Quit();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+Mix_Music *loadMusic(const char* path)
+{
+    Mix_Music *gMusic = Mix_LoadMUS(path);
+    if (gMusic == nullptr) {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+                       "Could not load music! SDL_mixer Error: %s", Mix_GetError());
+    }
+    return gMusic;
+}
+void playMusic(Mix_Music *gMusic)
+{
+    if (gMusic == nullptr) return;
+
+    if (Mix_PlayingMusic() == 0) {
+        Mix_PlayMusic( gMusic, -1 );
+    }
+    else if( Mix_PausedMusic() == 1 ) {
+        Mix_ResumeMusic();
+    }
+}
+
+Mix_Chunk* loadSound(const char* path) {
+    Mix_Chunk* gChunk = Mix_LoadWAV(path);
+    if (gChunk == nullptr) {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+                   "Could not load sound! SDL_mixer Error: %s", Mix_GetError());
+    }
+}
+void playChunk(Mix_Chunk* gChunk) {
+    if (gChunk != nullptr) {
+        Mix_PlayChannel( -1, gChunk, 0 );
+    }
+}
+
+
+
+#endif // GRAPHICS_H_INCLUDED
